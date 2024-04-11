7032b84685aa1c6b3624591cc4d55bfa190a41af
Dong In Kang <ehddls020624@gmail.com>
2024-04-11 10:29:15 +0900

    tailwind.config.js change 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
