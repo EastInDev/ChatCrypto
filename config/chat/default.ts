cc17c27cde5b9b1beb611ed806fbd1562cbdb4ea
Dong In Kang <ehddls020624@gmail.com>
2024-04-11 10:51:48 +0900

    server url change

--- config/chat/default.ts
+++ config/chat/default.ts
@@ -1,2 +1,3 @@
 export const SOCKET_URL =
-  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
+  // process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_SOCKET_URL || "https://chat-crypto-server.vercel.app";
